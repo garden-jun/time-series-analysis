9a2e56c0d894f9371a9dd812e5ff722c28ed1608
정원준 <79080825+garden-jun@users.noreply.github.com>
2023-03-28 21:50:33 +0900

    hw2.R 추가

--- chapter1/hw2.R
+++ chapter1/hw2.R
@@ -1,1 +1,17 @@
-source("C:/data/hw2.R")
+
+file.access("C:/Users/Gardenjun/Desktop/data",2)
+source("C:/Users/Gardenjun/Desktop/data")
+
+
+data <- read.table("C:/Users/Gardenjun/Desktop/data/usapass_hw1.txt")
+n <- nrow(data)
+t <- 1:n
+
+df <- data.frame(t, data)
+df
+
+
+# 데이터 그래프 그리기
+plot(df, type="l", ylab="usapass")
+
+
